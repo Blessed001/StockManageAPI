c7af960e10378eb553a78a2678215e3c4aeaf630
Dio.Code <claudiojoc46@mail.ru>
2020-03-24 03:21:02 +0300

    Moved IOperationTypeRepository and OperationTypeRe

--- StockManageAPI/Startup.cs
+++ StockManageAPI/Startup.cs
@@ -32,6 +32,8 @@ namespace StockManageAPI
             options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddTransient<SeedDb>();
 
+            
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
